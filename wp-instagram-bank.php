87e1e793169fa92d3391d52b488a4ed806d7b735
contact-banker <contact-banker@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-07-25 12:02:26 +0000

    update description
    git-svn-id: https://plugins.svn.wordpress.org/wp-instagram-bank/trunk@954732 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- wp-instagram-bank.php
+++ wp-instagram-bank.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: Wp Instagram Bank
 Plugin URI: http://tech-banker.com
-Description: 
+Description: WP Instagram Bank is an ultimate WordPress Plugin to showcase your latest Instagram pics.
 Author: Tech Banker
 Version: 1.0
 Author URI: http://tech-banker.com
